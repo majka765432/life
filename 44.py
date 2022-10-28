02076a9decbb116c6b6e7d3dd79391b6fa2493f4
majka765432 <115864634+majka765432@users.noreply.github.com>
2022-10-28 19:42:31 +0300

    life

---
+++ 44.py
@@ -0,0 +1,18 @@
+class Chelovek:
+    height = 220
+    satiety = 100
+    age = 25
+    eyecolor = "zelenskuy"
+    emotions = "sad"
+class Est(Chelovek):
+    setiety = 110
+    emotions = "radostnuy"
+    age = 30
+class Worker(Chelovek):
+    setiety = 50
+    emotions = "cry"
+class Sleep(Chelovek):
+    emotions = "super radostnuy"
+    height = 221
+
+#он сварщик просто
